7af52a57ba69760be51f06fa26967272452b2318
rlqja9711 <rlqja9711@gmail.com>
2022-10-13 13:56:02 +0900

    fix_Fireman

--- Client/Header/FireMan.h
+++ Client/Header/FireMan.h
@@ -30,33 +30,32 @@ private:
 	CCubeCol*			m_pAnimationBox = nullptr;
 
 	_float				m_fFrame = 0.f;
+	_bool				m_bRun = false;
 
 private:	//	애니메이션 관련
-	HRESULT					Build(void);
-	void					Load_Animation(wstring FileName);
-	void					Run_Animation(const _float& AnimationSpeed);
-	void					Look_Direction(void);
+	HRESULT				Build(void);
+	void				Load_Animation(wstring FileName, _uint AnimationID);
+	void				Run_Animation(const _float& AnimationSpeed);
+	void				Look_Direction(void);
 
-	void					Walk_Animation_Run(void);
-	void					Idle_Animation_Run(void);
-	void					Attack_Animation_Run(void);
+	_float				m_fTimeDelta = 0.f;
+	_bool				m_bFirst = true;
+	_float				m_AnimationTime = 0.f;
 
-	_float					m_fTimeDelta = 0.f;
-	_bool					m_bFirst = true;
-	_float					m_AnimationTime = 0.f;
-	_bool					m_bStateChange = false;
+	void				Walk_Animation_Run(void);
+	void				Idle_Animation_Run(void);
+	void				Attack_Animation_Run(void);
 
-	FIREMANSTATEID			m_STATE;
-	FIREMANSTATEID			m_BeforeState;
-	FIREMANWALKID			m_WALK;
-	FIREMANIDLEID			m_IDLE;
-	FIREMANATTACKID			m_ATTACK;
+	FIREMANSTATEID		m_STATE;
+	FIREMANSTATEID		m_BeforeState;
+	FIREMANWALKID		m_WALK;
+	FIREMANIDLEID		m_IDLE;
+	FIREMANATTACKID		m_ATTACK;
 
-	CLayer*					pMyLayer;
-	_tchar*					m_MonsterName;
+	CLayer*				pMyLayer;
+	_tchar*				m_MonsterName;
 
-	list<char*>				m_CharList;
-	list<_tchar*>			m_TcharList;
+	list<_tchar*>		m_TcharList;
 
 public:
 	static CFireMan*	Create(LPDIRECT3DDEVICE9 pGraphicDev, const _vec3& vPos, _tchar* Name);
