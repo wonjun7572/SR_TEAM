900930430e61b13bc202c7c602c5a9e5d056418b
beombeom12 <bumsuk509@naver.com>
2022-10-13 10:35:51 +0900

    Update(Middle)

--- Client/Code/Stage.cpp
+++ Client/Code/Stage.cpp
@@ -66,6 +66,7 @@
 #include "MonsterParticle.h"
 #include "ProjectileParticle.h"
 #include "Illusioner.h"
+#include "MiddleBoss.h"
 CStage::CStage(LPDIRECT3DDEVICE9 pGraphicDev)
 	: Engine::CScene(pGraphicDev)
 {
@@ -442,6 +443,11 @@ HRESULT CStage::Ready_Layer_Monster(const _tchar * pLayerTag)
 		NULL_CHECK_RETURN(pGameObject, E_FAIL);
 		FAILED_CHECK_RETURN(pLayer->Add_GameList(pGameObject), E_FAIL);
 	}
+
+
+	pGameObject = CMiddleBoss::Create(m_pGraphicDev, _vec3(109.f, 0.6f, 10.f ));
+	NULL_CHECK_RETURN(pGameObject, E_FAIL);
+	FAILED_CHECK_RETURN(pLayer->Add_GameList(pGameObject), E_FAIL);
 
 
 	m_mapLayer.insert({ pLayerTag, pLayer });
