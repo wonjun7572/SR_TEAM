6ee14df00f4da7a953ef300f5fea6a957adbb583
Wonjun_choi <54612463+wonjun7572@users.noreply.github.com>
2022-10-23 13:46:23 +0900

    Update Npc.h

--- Client/Header/Npc.h
+++ Client/Header/Npc.h
@@ -20,7 +20,11 @@ private:
 	void				Quest1(const _float& fTimeDelta);
 	void				Quest2(const _float& fTiemDelta);
 	void				Quest3(const _float& fTiemDelta);
+<<<<<<< HEAD
 	void				Finish(const _float& fTimeDelta);
+=======
+	void				Projection_Effect(void);
+>>>>>>> main
 
 private:
 	CRcTex*				m_pBufferCom = nullptr;
@@ -55,6 +59,10 @@ private:
 	_bool				m_bQuestText4 = false;
 
 	_bool				m_bBossDead = false;
+
+	_bool				m_bEffect1 = false;
+	_bool				m_bEffect2 = false;
+	_bool				m_bEffect3 = false;
 
 public:
 	static	CNpc*	Create(LPDIRECT3DDEVICE9 pGraphicDev, const _vec3& vPos);
